54d4d64b8b8a17c6e6a68be846d5e5b83cb156b8
Zahidur Talukder <zahidur.eee.buet@gmail.com>
2023-06-29 15:56:41 -0500

    test3

--- utils/mnist_data_generator.py
+++ utils/mnist_data_generator.py
@@ -6,7 +6,7 @@ import pickle
 
 
 def mnist_shuffle_data(client_percent, data_percent, num_clients):    # Get the data.
-    (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
     x_train = x_train.reshape(60000, 784)
     x_test = x_test.reshape(10000, 784)
     x_train = x_train.astype('float32')
@@ -14,7 +14,7 @@ def mnist_shuffle_data(client_percent, data_percent, num_clients):    # Get the 
     x_train /= 255
     x_test /= 255
     nb_classes= 10
-    y_test = keras.utils.to_categorical(y_test, nb_classes)
+    y_test = tf.keras.utils.to_categorical(y_test, nb_classes)
     dataset, client_names= create_client(x_train, y_train, num_clients, initial='client')
 
 
@@ -145,6 +145,29 @@ def mnist_flip_data(client_percent, data_percent, num_clients):    # Get the dat
     # open_file.close()
     return sample_list
 
+def mnist_noniid_noise_data(client_percent, data_percent, num_clients):    # Get the data.
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+    x_train = x_train.reshape(60000, 784)
+    x_test = x_test.reshape(10000, 784)
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+    x_train /= 255
+    x_test /= 255
+    nb_classes= 10
+    y_test = tf.keras.utils.to_categorical(y_test, nb_classes)
+    dataset, client_names= create_non_iid_mnist(x_train, y_train, num_clients, percent=.5,initial='noisy_clients')
+
+    # Creating bad clients
+    clients, bad_client = creating_noisy_clients_mnist(dataset,client_names,client_percent, data_percent)
+
+    sample_list=[clients, bad_client, x_test, y_test]
+    # file_name = f"../../Data/mnist/Dataset{client_percent}_{data_percent}_{num_clients}_noise_mnist.pkl"
+    #
+    # open_file = open(file_name, "wb")
+    # pickle.dump(sample_list, open_file)
+    # open_file.close()
+    return sample_list
+
 def mnist_noise_data(client_percent, data_percent, num_clients):    # Get the data.
     (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
     x_train = x_train.reshape(60000, 784)
@@ -161,8 +184,9 @@ def mnist_noise_data(client_percent, data_percent, num_clients):    # Get the da
     clients, bad_client = creating_noisy_clients_mnist(dataset,client_names,client_percent, data_percent)
 
     sample_list=[clients, bad_client, x_test, y_test]
-    file_name = f"../../Data/mnist/Dataset{client_percent}_{data_percent}_{num_clients}_noise_mnist.pkl"
-
-    open_file = open(file_name, "wb")
-    pickle.dump(sample_list, open_file)
-    open_file.close()
+    # file_name = f"../../Data/mnist/Dataset{client_percent}_{data_percent}_{num_clients}_noise_mnist.pkl"
+    #
+    # open_file = open(file_name, "wb")
+    # pickle.dump(sample_list, open_file)
+    # open_file.close()
+    return sample_list
